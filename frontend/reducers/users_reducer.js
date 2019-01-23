051b8b51e4e3fd1e974d85f5fbcd841056b56d00
Nadav <nbassell@gmail.com>
2019-01-22 20:15:54 -0500

    finish session and users reducers

---
+++ frontend/reducers/users_reducer.js
@@ -0,0 +1,16 @@
+import { RECEIVE_CURRENT_USER, LOGOUT_CURRENT_USER } from '../actions/session_actions';
+import merge from 'lodash/merge';
+
+const usersReducer = (oldState = {}, action) => {
+  Object.freeze(oldState);
+  switch (action.type) {
+    case RECEIVE_CURRENT_USER:
+      return merge({}, oldState, { [action.currentUser.id]: action.currentUser });
+    case LOGOUT_CURRENT_USER:
+      return {};
+    default:
+      return oldState;
+  }
+};
+
+export default usersReducer;
