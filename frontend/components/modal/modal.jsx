a16a46b236c5b88361ee7e8ec2a38a16305fb061
Nadav <nbassell@gmail.com>
2019-02-15 20:23:57 -0500

    create review modal, refactor session modals

--- frontend/components/modal/modal.jsx
+++ frontend/components/modal/modal.jsx
@@ -3,24 +3,30 @@ import LoginFormContainer from "../session_form/login_form_container";
 import SignupFormContainer from "../session_form/signup_form_container";
 import CreateReviewForm from '../review_form/create_review_container';
 import EditReviewForm from '../review_form/edit_review_container';
+import { Route } from 'react-router-dom';
 
-
-export default ({ modal, closeModal }) => {
+export default ({ modal, closeModal, reviewId }) => {
   if (!modal) {
     return null;
   }
   let component;
   switch (modal) {
     case 'login':
-      component = <LoginFormContainer />;
+      component = <LoginFormContainer formType={modal} />;
       break;
     case 'signup':
-      component = <SignupFormContainer />;
+      component = <SignupFormContainer formType={modal} />;
       break;
     case 'create review':
-      component = <CreateReviewForm />
+      return (
+        <div className="modal-background" onClick={ closeModal }>
+          <div className="modal-child" onClick={ e => e.stopPropagation() }>
+            <Route component={ ReviewForm } path="/restaurants/:id"/>
+          </div>
+        </div>
+      );
     case 'edit review':
-      component = <EditReviewForm />;
+      component = <EditReviewForm reviewId={reviewId} />;
     default:
       return null;
   }
@@ -33,3 +39,43 @@ export default ({ modal, closeModal }) => {
     </div>
   );
 }
+
+// import React from 'react';
+// import LoginFormContainer from "../session_form/login_form_container";
+// import SignupFormContainer from "../session_form/signup_form_container";
+// import CreateReviewForm from '../review_form/create_review_container';
+// import EditReviewForm from '../review_form/edit_review_container';
+
+// export default ({ modal, reviewId, closeModal }) => {
+//   if (!modal) {
+//     return null;
+//   }
+
+//   switch(modal) {
+//     case "create review":
+//       return (
+//         <div className="modal-background" onClick={ closeModal }>
+//           <div className="modal-child" onClick={ e => e.stopPropagation() }>
+//             <Route component={ ReviewForm } path="/restaurants/:id"/>
+//           </div>
+//         </div>
+//       );
+//     case "edit review":
+//     return (
+//       <div className="modal-background" onClick={ closeModal }>
+//         <div className="modal-child" onClick={ e => e.stopPropagation() }>
+//           <EditReviewForm reviewId={ reviewId }/>
+//         </div>
+//       </div>
+//     );
+//     case "login":
+//     case "signup":
+//       return (
+//         <div className="modal-background" onClick={ closeModal }>
+//           <div className="modal-child" onClick={ e => e.stopPropagation() }>
+//             <SessionFormContainer formType={ modal }/>
+//           </div>
+//         </div>
+//       );
+//   }
+// };
--- frontend/components/review_form/review_form.jsx
+++ frontend/components/review_form/review_form.jsx
@@ -6,6 +6,7 @@ export default class ReviewForm extends React.Component {
     super(props);
     this.state = this.initialState();
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleMouseLeave = this.handleMouseLeave.bind(this);
   }
 
   componentWillUnmount() {
@@ -16,7 +17,164 @@ export default class ReviewForm extends React.Component {
     if (this.props.formType === 'create') {
       return {
         overall_rating: 0,
-      }
+        food_rating: 0,
+        service_rating: 0,
+        ambience_rating: 0,
+        value_rating: 0,
+        body: "",
+
+        hovering: null,
+        hoverValue: null
+      };
+    } else {
+      const review = this.props.review;
+      return {
+        id: review.id,
+        overall_rating: review.overall_rating,
+        food_rating: review.food_rating,
+        service_rating: review.service_rating,
+        ambience_rating: review.ambience_rating,
+        value_rating: review.value_rating,
+        body: review.body,
+
+        hovering: null,
+        hoverValue: null
+      };
     }
   }
+
+  updateField(field) {
+    return (e) => {
+      this.props.clearReviewErrors();
+      this.setState({[field]: e.currentTarget.value});
+    };
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const { currentUser, restaurant, processForm } = this.props;
+    const review = merge({}, this.state, {
+      user_id: currentUser.id,
+      restaurant_id: restaurant.id,
+    });
+    delete review.hovering;
+    delete review.hoverValue;
+    processForm(review);
+  }
+
+  updateRating(ratingType, newRating) {
+    return (e) => {
+      this.props.clearReviewErrors();
+      this.setState({[ratingType]: newRating });
+    }
+  }
+
+  handleMouseEnter(ratingType, i) {
+    return (e) => this.setState({
+      hovering: ratingType,
+      hoverValue: i
+    });
+  }
+
+  handleMouseLeave() {
+    this.setState({
+      hovering: null,
+      hoverValue: null
+    });
+  }
+
+  errorMessages() {
+    const errors = this.props.errors;
+    if (errors.length === 0) {
+      return null;
+    } else {
+      const errorLis = errors.map((error, i) => (
+        <li className="review-error-text" key={idx}>{error}</li>
+      ));
+      
+      return (
+        <ul className="review-error-container">
+        {errorLis}
+      </ul>
+      )
+    }
+  }
+
+  ratingStars(ratingType) {
+    let stars = [];
+    for (let i = 1; i < 5; i++) {
+      let colorThreshold = this.state.hovering == ratingType ?
+        this.state.hoverValue : this.state[ratingType];
+      let status = (i <= colorThreshold) ? "full-star" : "";
+      stars.push(
+        <i key={i} className="fa fa-star" id={status} aria-hidden="true"
+           onMouseEnter={this.handleMouseEnter(ratingType, i)}
+           onMouseLeave={this.handleMouseLeave}
+           onClick={this.updateRating(ratingType, i)}></i>
+      );
+    }
+    return stars;
+  }
+
+  render() {
+    const { restaurant, currentUser } = this.props;
+
+    return (
+      <div className="review-form-container">
+        <button onClick={this.props.closeModal}
+          className="review-close-button">&times;</button>
+        <form className="review-form" onSubmit={ this.handleSubmit }>
+
+          <h3 className="review-header">
+            { `${currentUser.fname}, how was your dining experience at ${restaurant.name}?` }
+          </h3>
+          { this.errorMessages() }
+          <div className="ratings-container">
+            <label>Overall Rating
+              <div className="star-container">
+                { this.ratingStars("overall_rating") }
+              </div>
+            </label>
+
+            <label>Food
+              <div className="star-container">
+                { this.ratingStars("food_rating") }
+              </div>
+            </label>
+
+            <label>Service
+              <div className="star-container">
+                { this.ratingStars("service_rating") }
+              </div>
+            </label>
+
+            <label>Ambience
+              <div className="star-container">
+                { this.ratingStars("ambience_rating") }
+              </div>
+            </label>
+
+            <label>Value
+              <div className="value-container">
+                { this.ratingStars("value_rating") }
+              </div>
+            </label>
+          </div>
+
+          <div className="review-body-container">
+            <textarea
+              className="review-body"
+              placeholder="Please leave a brief description of your experience."
+              value={ this.state.body }
+              onChange={this.updateField('body')}
+            />
+            { this.recommendationCheckbox() }
+          </div>
+
+          <input type="submit" value="Submit Review" className="review-submit"/>
+        </form>
+      </div>
+    );
+  }
+
 }
