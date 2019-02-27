f04782666e2e589ae43d34743bc094e1518df0d8
Nadav <nbassell@gmail.com>
2019-02-27 14:03:04 -0500

    make personal links open in a new tab

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Scopin'Tables
 
 [![forthebadge](https://forthebadge.com/images/badges/made-with-ruby.svg)](https://forthebadge.com)
+[![forthebadge](https://forthebadge.com/images/badges/made-with-javascript.svg)](https://forthebadge.com)
 [![forthebadge](https://forthebadge.com/images/badges/built-with-love.svg)](https://forthebadge.com)
 
 [Live Demo][heroku]
--- frontend/components/main/footer.jsx
+++ frontend/components/main/footer.jsx
@@ -14,10 +14,10 @@ const Footer = () => {
           <p>Inspired by the web application OpenTable.</p>
         </div>
         <ul className="footer-links">
-          <li><a href="https://nbassell.github.io/">{<FaCode className="icon" size={15}/>}<span>Personal Site</span></a></li>
-          <li><a href="https://github.com/nbassell">{<FaGithub className="icon" size={15}/>}<span>Github</span></a></li>
-          <li><a href="https://www.linkedin.com/in/nbassell">{<FaLinkedin className="icon" size={15}/>}<span>LinkedIn</span></a></li>
-          <li><a href="https://angel.co/nadav-bassell">{<FaAngellist className="icon" size={15}/>}<span>AngelList</span></a></li>
+          <li><a href="https://nbassell.github.io/" target="_blank">{<FaCode className="icon" size={15}/>}<span>Personal Site</span></a></li>
+          <li><a href="https://github.com/nbassell" target="_blank">{<FaGithub className="icon" size={15}/>}<span>Github</span></a></li>
+          <li><a href="https://www.linkedin.com/in/nbassell" target="_blank">{<FaLinkedin className="icon" size={15}/>}<span>LinkedIn</span></a></li>
+          <li><a href="https://angel.co/nbassell" target="_blank">{<FaAngellist className="icon" size={15}/>}<span>AngelList</span></a></li>
         </ul>
       </div>
     </footer>
