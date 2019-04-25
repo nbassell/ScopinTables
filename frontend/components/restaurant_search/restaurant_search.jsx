f2e9f47e79f031ded84a7f79269fb97eb6355d85
Nadav <nbassell@gmail.com>
2019-04-25 12:54:03 -0400

    change search placeholder

--- frontend/components/restaurant_search/restaurant_search.jsx
+++ frontend/components/restaurant_search/restaurant_search.jsx
@@ -160,7 +160,7 @@ class RestaurantSearch extends React.Component {
                         type="text"
                         onChange={this.updateField}
                         value={this.state.search_term}
-                        placeholder="Location, Restaurant, or Cuisine"/>
+                        placeholder="Restaurant or Cuisine (try 'Seafood'!)"/>
                 </div>
                 <div id="search-dropdown" className={`search-dropdown ${showing}`}>
                   <ul>
